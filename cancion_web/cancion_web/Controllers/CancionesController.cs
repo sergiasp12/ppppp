2b55d684d9942e9336267f8c7ebd6cc0ca97cbfe
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 12:24:37 -0400

    finalizado

---
+++ cancion_web/cancion_web/Controllers/CancionesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cancion_web.Data;
+using cancion_web.Models;
+
+namespace cancion_web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CancionesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CancionesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Canciones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<cancion>>> Getcancion()
+        {
+            return await _context.cancion.ToListAsync();
+        }
+
+        // GET: api/Canciones/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<cancion>> Getcancion(string id)
+        {
+            var cancion = await _context.cancion.FindAsync(id);
+
+            if (cancion == null)
+            {
+                return NotFound();
+            }
+
+            return cancion;
+        }
+
+        // PUT: api/Canciones/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putcancion(string id, cancion cancion)
+        {
+            if (id != cancion.Nombre)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cancion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!cancionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Canciones
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<cancion>> Postcancion(cancion cancion)
+        {
+            _context.cancion.Add(cancion);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (cancionExists(cancion.Nombre))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("Getcancion", new { id = cancion.Nombre }, cancion);
+        }
+
+        // DELETE: api/Canciones/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletecancion(string id)
+        {
+            var cancion = await _context.cancion.FindAsync(id);
+            if (cancion == null)
+            {
+                return NotFound();
+            }
+
+            _context.cancion.Remove(cancion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool cancionExists(string id)
+        {
+            return _context.cancion.Any(e => e.Nombre == id);
+        }
+    }
+}
