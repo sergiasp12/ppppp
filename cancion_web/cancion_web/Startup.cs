286af2355608bc4d7569d4fc887ebd4100d96a44
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 09:55:36 -0400

    DbContext agreagado a startup

--- cancion_web/cancion_web/Startup.cs
+++ cancion_web/cancion_web/Startup.cs
@@ -1,3 +1,4 @@
+using cancion_web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace cancion_web
 {
@@ -26,6 +28,8 @@ namespace cancion_web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
