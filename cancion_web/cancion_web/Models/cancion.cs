ed47069827d1cd98625180d0e4599f56a64753e0
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 02:09:50 -0400

    primer commit

---
+++ cancion_web/cancion_web/Models/cancion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cancion_web.Models
+{
+    public class cancion
+    {
+        [Key]
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings = false , ErrorMessage = "{0} es requerido")]
+        public string autor { get; set; }
+        [StringLength(200, MinimumLength = 10 , ErrorMessage ="{0} es requerido")]
+        public string letra { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 10 , ErrorMessage ="la longitud de {0} debe estar entre {2} y {1}")]
+        public string enlace { get; set; }
+        
+
+
+    }
+}
