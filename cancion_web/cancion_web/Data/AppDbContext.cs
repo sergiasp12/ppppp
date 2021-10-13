e084b7149645d0b529cc0888e82245e1b4bfbad1
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 09:47:07 -0400

    conecion agregado

---
+++ cancion_web/cancion_web/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using cancion_web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cancion_web.Data
+{
+    
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<cancion> cancion { get; set; }
+
+        
+    }
+}
